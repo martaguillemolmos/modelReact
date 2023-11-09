32b1ef79163ca5dd18d2df8b0d87376b1cff9691
martaguillemolmos <martetaguillem@gmail.com>
2023-11-09 16:46:41 +0100

    feat: Create the Body structure, where, in each of the routes that we are going to export, it will correspond to one of the pages that we have created.

---
+++ tattostudio/src/pages/Body/Body.jsx
@@ -0,0 +1,11 @@
+import {Routes, Route} from 'react-router-dom'
+
+export const Body = () => {
+    return (
+        <>
+        <Routes>
+            <Route />
+        </Routes>
+        </>
+    )
+}
